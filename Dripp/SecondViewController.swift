4f8604e56ad8ae15a36406178d265a148fdf5bea
Henry Saniuk <henry@henrysaniuk.com>
2016-01-28 23:53:40 -0500

    Initial commit

---
+++ Dripp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Dripp
+//
+//  Created by Henry Saniuk on 1/28/16.
+//  Copyright © 2016 Henry Saniuk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

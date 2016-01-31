eee9a46b1b4fc648b3d2ab8e7ac00157811420f3
Bradon Hudson <bhud07@gmail.com>
2016-01-31 04:07:58 -0500

    Update Playlists

--- Dripp/ShowerViewController.swift
+++ Dripp/ShowerViewController.swift
@@ -25,8 +25,8 @@ class ShowerViewController: UITableViewController {
     
     func loadPlaylists() {
         playlists.append(Playlist(name: "Starred", duration: "15m", albumArtwork: "starred", recommended: true))
+        playlists.append(Playlist(name: "Top Hits", duration: "2h51m", albumArtwork: "tophits", recommended: false))
         playlists.append(Playlist(name: "Discover Weekly", duration: "1h54m", albumArtwork: "discoverweekly", recommended: false))
-        playlists.append(Playlist(name: "Top Hits", duration: "2h57m", albumArtwork: "tophits", recommended: false))
         playlists.append(Playlist(name: "Songs of Innocence", duration: "49m", albumArtwork: "u2", recommended: false))
         playlists.append(Playlist(name: "Power Shower", duration: "17m", albumArtwork: "saintmotel", recommended: false))
         playlists.append(Playlist(name: "All You Need is Love", duration: "2h15m", albumArtwork: "beatles", recommended: false))
